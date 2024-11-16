8fd585bcd37371369b73a203bf4a8b551a0f8333
Mahach22 <104731301+Mahach22@users.noreply.github.com>
2024-11-17 00:19:45 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -8,7 +8,7 @@ def print_hello():
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2024, 11, 17),
+    'start_date': datetime(2024, 11, 16),
     'email_on_failure': False,
     'email_on_retry': False,
 }
